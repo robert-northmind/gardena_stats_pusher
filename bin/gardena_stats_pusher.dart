0a6afb0583134985cba1bd5c8546d78628c5c08e
Robert Magnusson <robert@northmind.io>
2024-04-24 23:50:51 +0200

    Update action to use secrets

--- bin/gardena_stats_pusher.dart
+++ bin/gardena_stats_pusher.dart
@@ -2,5 +2,9 @@ import 'package:gardena_stats_pusher/gardena_stats_pusher.dart'
     as gardena_stats_pusher;
 
 void main(List<String> arguments) {
-  gardena_stats_pusher.main();
+  try {
+    gardena_stats_pusher.main();
+  } catch (error) {
+    print('gardena_stats_pusher failed with error: $error');
+  }
 }
